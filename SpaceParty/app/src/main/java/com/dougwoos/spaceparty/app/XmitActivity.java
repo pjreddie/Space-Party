a07c8be1251ea6788795687d631bd24e366a8800
Joseph Redmon <pjreddie@start9.(none)>
2014-04-20 16:37:43 -0700

    HAHAHAHAHAHAHAHAHAHAHA

--- SpaceParty/app/src/main/java/com/dougwoos/spaceparty/app/XmitActivity.java
+++ SpaceParty/app/src/main/java/com/dougwoos/spaceparty/app/XmitActivity.java
@@ -5,8 +5,35 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.nio.ByteBuffer;
+
 
 public class XmitActivity extends ActionBarActivity {
+    public byte[] encode(String message){
+        byte[] data = new byte[message.length() + 4];
+        ByteBuffer b = ByteBuffer.allocate(4);
+        b.putInt(message.length());
+        System.arraycopy(b.array(),0,data,0,4);
+        System.arraycopy(message.getBytes(), 0, data, 4, message.length());
+        return data;
+    }
+
+    public short[] bell202_modulate(byte[] data, int samplesperbit){
+        double markf = 1200;
+        double spacef = 2200;
+        short[] wave = new short[data.length*4*samplesperbit];
+        int t = 0;
+        for(int i = 0; i < data.length; ++i){
+            byte b = data[i];
+            for(int j = 0; j < 4; ++j){
+                int bit = (b >> j) & 1;
+                for(int k = 0; k < samplesperbit; ++k){
+                    wave[t++] = (short)(Math.sin(1)*Short.MAX_VALUE);
+                }
+            }
+        }
+        return wave;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
